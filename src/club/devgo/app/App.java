c7a5d198bb206d23a1978e7ba7c4213a9892c72c
dev-go <dev-go@users.noreply.github.com>
2020-12-18 13:00:54 +0800

    init project

---
+++ src/club/devgo/app/App.java
@@ -0,0 +1,10 @@
+// Copyright (c) 2020, devgo.club
+// All rights reserved.
+
+package club.devgo.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Algorithm-DataStructure!");
+    }
+}
